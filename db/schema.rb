7d36a1dc0b490bfdc9284652afa35d5de61d4e14
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-25 03:19:35 -0700

    Adding Trackable to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_213116) do
+ActiveRecord::Schema.define(version: 2020_07_25_101203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
