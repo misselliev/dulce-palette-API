6a9f828669f1c69557f90061a071ed0880d27dc3
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-31 20:25:08 -0700

    Updating routes to include favorites

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :palettes
+      resources :favorites
     end
   end
 end
